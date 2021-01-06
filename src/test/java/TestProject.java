be5a646b8ef7e902b6bf5d64158d219c8b9f8737
bozi0822 <bozi0822@qq.com>
2021-01-07 01:10:28 +0800

    first project for ApiDemo

--- src/test/java/TestProject.java
+++ src/test/java/TestProject.java
@@ -1,10 +1,17 @@
 import static org.junit.Assert.*;
 
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
 import com.bob.api.CommonResult;
 import com.bob.api.ParameterRes;
+import com.google.gson.Gson;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.junit.Test;
 
 public class TestProject {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestProject.class);
     @Test
     public void testRunApi(){
         ParameterRes parameterRes = new ParameterRes();
@@ -12,7 +19,15 @@ public class TestProject {
         parameterRes.setCurrent("20 A");
         parameterRes.setTemperature("100 deg");
 
-        System.out.println(CommonResult.success(parameterRes));
+        String json = JSONUtil.parse(CommonResult.success(parameterRes)).toString();
+        Gson gson = new Gson();
+        String json1 = gson.toJson(CommonResult.success(parameterRes));
+        String json2 = JSON.toJSONString(CommonResult.success(parameterRes));
+        System.out.println(json);
+        System.out.println(json1);
+        System.out.println(json2);
+
+        System.out.println();
         System.out.println(CommonResult.failed());
         System.out.println(CommonResult.failed());
     }
